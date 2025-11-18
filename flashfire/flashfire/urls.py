a0400bcbc412a8e72752e189dd1a100d8867df53
safna555 <safnam07122003@gmail.com>
2025-11-18 15:23:43 +0530

    initialised

--- flashfire/flashfire/urls.py
+++ flashfire/flashfire/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
-]
+    path('', views.home, name='home'),
+    path('base/', views.base, name='base')
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- flashfire/main/views.py
+++ flashfire/main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/index.html')
+
+def base(request):
+    return render(request, 'core/base.html')
